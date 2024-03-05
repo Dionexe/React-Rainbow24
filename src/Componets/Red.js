ce23915b5160d8bd0801f2dd030ebd9230fceaec
Dion Wells <dionwells@Dions-MacBook-Pro.local>
2024-03-04 18:13:18 -0800

    second commit

---
+++ src/Componets/Red.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Red = (props) => {
+    return (
+        <div className="Red">
+            <h1 style ='color: red;'> Red </h1>
+        </div>
+    )
+}
+
+export default Red
